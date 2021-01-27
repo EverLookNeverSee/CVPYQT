2a1cf2304c6c65566c8864af58cae591b893e516
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-27 09:40:29 +0330

    importing necessary modules

--- cv/example_01.py
+++ cv/example_01.py
@@ -2,3 +2,10 @@
 This example Demonstrates how to convert and display images loaded from
 OpenCV onto QLabel widgets
 """
+
+# import necessary modules
+import sys, os, cv2
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QLabel, QFileDialog,
+                             QMessageBox, QHBoxLayout, QVBoxLayout, QAction)
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt
